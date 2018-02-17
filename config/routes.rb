7b89b27bbe0570c15f836a3198220f715dfd622f
Mahesh <mahesh.g@live.com>
2018-02-17 14:13:57 +0530

    Added meetings section

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :meetings, :path => 'admin/meetings'
   resources :members, :path => 'admin/members'
+
+  get  '/meetings/active', to: 'meetings#next_active'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180217062224) do
+ActiveRecord::Schema.define(version: 20180217072114) do
+
+  create_table "meetings", force: :cascade do |t|
+    t.date "start_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "members", force: :cascade do |t|
     t.string "name"
