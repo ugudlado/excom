09510962bcdd965e2e1212e021bb792a40923fc7
Mahesh <mahesh.g@live.com>
2018-07-22 16:10:36 +0530

    Updated migration script to remove password field

--- db/migrate/20180721085710_add_status_and_password_to_member.rb
+++
@@ -1,6 +0,0 @@
-class AddStatusAndPasswordToMember < ActiveRecord::Migration[5.1]
-  def change
-    add_column :members, :password, :password, :default => ''
-    add_column :members, :status, :boolean, :default => true
-  end
-end
