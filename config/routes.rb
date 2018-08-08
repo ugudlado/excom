3f45d606d2e36346652dccc0ce5f23bc42580555
Mahesh <mahesh.g@live.com>
2018-08-08 16:48:20 +0530

    Updated member and devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,17 +8,15 @@ Rails.application.routes.draw do
     end
   end
   
-  resources :members, :path => '/admin/members'
+  resources :members
 
   root to: 'meetings#next_active'
   post '/meetings/validate_voter_id', to: 'meetings#validate_voter_id'
 
-  devise_scope :user do
-    get "/sign_up" => "members#new", as: "new_member_registration" # custom path to sign_up/registration
-    get "/edit" => "members#edit", as: "edit_member_registration" # custom path to sign_up/registration
+  devise_scope :member do
+    get "/sign_up" => "members#new", :as => "new_member_registration_path" # custom path to sign_up/registration
   end
 
-  devise_for :members
-
+  devise_for :member, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
